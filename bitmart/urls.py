2610079618176a556119e909dafdb234f295ad3d
Carl Henrik Lunde <chlunde@ping.uio.no>
2013-04-04 19:57:51 +0200

    o Expose products using tastypie

--- bitmart/urls.py
+++ bitmart/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import patterns, include, url
 from bitmart import settings
+from cashregister.api import ProductResource
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
+
+product_resource = ProductResource()
 
 urlpatterns = patterns('',
     url(r'^store/', include('cashregister.urls')),
@@ -14,4 +17,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    (r'^api/', include(product_resource.urls)),
 )
